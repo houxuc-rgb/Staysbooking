345f3d8b1f1b7db4043ebdd39eb235c1efe5cca8
houxuc-rgb <houxuc@andrew.cmu.edu>
2026-04-30 23:53:00 -0400

    Import missing backend sources

---
+++ backend/src/main/java/com/laioffer/staybooking/model/BookingDto.java
@@ -0,0 +1,27 @@
+package com.laioffer.staybooking.model;
+
+
+import com.laioffer.staybooking.model.BookingEntity;
+import com.laioffer.staybooking.model.ListingDto;
+import com.laioffer.staybooking.model.UserDto;
+
+import java.time.LocalDate;
+
+
+public record BookingDto(
+        Long id,
+        LocalDate checkInDate,
+        LocalDate checkOutDate,
+        UserDto guest,
+        ListingDto listing
+) {
+    public BookingDto(BookingEntity entity) {
+        this(
+                entity.getId(),
+                entity.getCheckInDate(),
+                entity.getCheckOutDate(),
+                new UserDto(entity.getGuest()),
+                new ListingDto(entity.getListing())
+        );
+    }
+}
